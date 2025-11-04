60c59ef9ee2f562fba334b31439bb9ca52412a8f
kingjinyong <wlsdydwhswkf@gmail.com>
2025-11-04 16:38:24 +0900

    fix: 에러 제거

--- src/main/java/org/example/server/chatRoom/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.server.chatRoom.repository;
-
-import org.example.server.chat.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
