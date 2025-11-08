ff4659eee7f4cfac7a798688d86fb132c3dec015
kingjinyong <wlsdydwhswkf@gmail.com>
2025-11-09 00:02:32 +0900

    chore: 도커 설정을 위한 설정

--- src/main/java/org/example/server/config/RedisConfig.java
+++ src/main/java/org/example/server/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package org.example.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,10 +11,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host:redis}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory("localhost",6379);
-
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
